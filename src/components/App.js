43fc45f86150cc3e995714ffcd199a6e0572f66e
Mark <marksreese@gmail.com>
2022-06-08 16:28:02 -0700

    forked repo to push and add collaborators

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import MainContent from "./MainContent";
+import SideMenu from "./SideMenu";
+import TopMenu from "./TopMenu";
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return (
+  <div className="app-container">
+    <MainContent />
+    <TopMenu />
+    <SideMenu />
+  </div>
+  );
 }
 
 export default App;
